4842f3ec67789898465119e24444caf3fd900885
mlerall <45630885+mlerall@users.noreply.github.com>
2023-05-05 10:00:42 +0200

    Update streamlit_app.py
    Add Input box for API GET query for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,11 @@ streamlit.dataframe(fruits_to_show)
 # Disply fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 
 # normalize the json output 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
